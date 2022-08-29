bcc828efcbf51e53243ef56930c6e61661f99d40
ashishmeshram844 <112160710+ashishmeshram844@users.noreply.github.com>
2022-08-29 12:50:32 +0530

    Comments and docstrings addedd

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,14 @@ from django.contrib import messages
 
 
 class IndexView(View):
+    """
+    - This class contain the functionality of loading index page and creating new employee
+    """
     def get(self,request):
+        """
+        - This function fetch all employee data and display on page
+        - also shows the form for creating new employee entry
+        """
         try:
             all_emp = Employee.objects.all()[::-1]
             context = {'all_emp' : all_emp}
@@ -13,6 +20,9 @@ class IndexView(View):
         except Exception as e:
             return HttpResponse(e)
     def post(self,request):
+        """
+        - This function is responsible for creating new employee
+        """
         try:
             name = request.POST['name']
             mobile = request.POST['mobile']
@@ -27,6 +37,9 @@ class IndexView(View):
 
 
 def DeleteEmployee(request,emp_id):
+    """
+    - This function is responsible for deleting employee
+    """
     try:
         emp = Employee.objects.get(id = emp_id)
         emp.delete()
@@ -37,7 +50,13 @@ def DeleteEmployee(request,emp_id):
 
 
 class EditEmployeeView(View):
+    """
+    - This class is responsible to edit functionality on employees
+    """
     def get(self,request,emp_id):
+        """
+        - This function shows all employee list and show the update employee form
+        """
         try:
             sel_emp = Employee.objects.get(id = emp_id)
             all_emp = Employee.objects.all()[::-1]
@@ -50,6 +69,9 @@ class EditEmployeeView(View):
             return HttpResponse(e)
 
     def post(self,request,emp_id):
+        """
+        - This function is responsible for updating employee's details
+        """
         try:
             emp = Employee.objects.get(id = emp_id)
             emp.mobile = request.POST['mobile']
